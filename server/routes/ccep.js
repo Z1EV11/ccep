35ebf7afd10d98115c0204937529f7deaa4b7db8
“Fr4nkWong” <frankwong96@foxmail.com>
2022-11-13 21:20:17 +0800

    feature: add elementUI, cors

--- server/routes/ccep.js
+++ server/routes/ccep.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET ccep page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/upload', function(req, res, next) {
+  console.log(1);
+});
+
+router.post('/assess', function(req, res, next) {
+  res.send('ccep/assess');
 });
 
 module.exports = router;
